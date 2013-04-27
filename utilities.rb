03d539b700d0cf9b8eff665c2652e9ea7373ab06
Kim Stefan Lindholm <misc@dynevision.com>
2013-04-27 14:40:52 +0200

    Support applying recipes to an existing project

---
+++ utilities.rb
@@ -0,0 +1,24 @@
+# >---------------------------------------------------------------------------<
+#   Utilities with potential to make development more productive and fun.
+#   Applying this template to an existing project:
+#
+#     rake rails:template LOCATION=<path>/nifty-templates/utilities.rb
+#
+#   Run 'bundle install' if you get error "Could not find gem..."
+# >---------------------------------------------------------------------------<
+
+@template_path = File.dirname(__FILE__)
+apply "#{@template_path}/initial_setup.rb"
+
+# Set default values or leave empty to be prompted
+@preferences = {}
+
+@recipes = [
+  # Commit to repository
+  "git"
+]
+
+apply_recipes @recipes
+
+# Run bundler and callbacks
+apply_after_hooks
