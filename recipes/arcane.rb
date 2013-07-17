e700e56acdbbc043eed37ad05d5057d4b0a47705
Kim Stefan Lindholm <misc@dynevision.com>
2013-07-17 18:00:16 +0200

    Arcane recipe

---
+++ recipes/arcane.rb
@@ -0,0 +1,18 @@
+# >---------------------------------[ Arcane ]---------------------------------<
+
+@current_recipe = "arcane"
+@before_configs["arcane"].call if @before_configs["arcane"]
+say_recipe 'Arcane'
+
+
+@configs[@current_recipe] = config
+
+gem "arcane", "~> 1.0.0"
+
+initializer "arcane.rb", <<-RUBY
+ApplicationController.send :include, Arcane
+RUBY
+
+after_bundler do
+  generate 'arcane:install'
+end
